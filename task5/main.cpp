3b76dbbf7cd23b287e83fb3787f4e87230a3d048
xokc <xokc@mail.ru>
2017-10-02 15:34:39 +0300

    =Task5 completted

--- task5/main.cpp
+++ task5/main.cpp
@@ -1,4 +1,4 @@
-#define DEBUG_DEV
+#define _DEBUG_DEV
 
 #ifdef DEBUG_DEV
 #include <ctime>
@@ -13,9 +13,11 @@
 
 using namespace std;
 
+static list<string> script;
+
 void print(const string &name, const string &index)
 {
-    cout << "move " << name << " " << index << endl;
+    script.push_back("move " + name + " " + index);
 }
 
 int main(int argc, char **argv)
@@ -33,8 +35,7 @@ int main(int argc, char **argv)
     clock_t beg = clock();
 #endif
 
-    map<string, int> testa;
-    set<string> tests[2];
+    map<string, int> tests;
     list<string> good;
     list<string> bad[2];
 
@@ -46,77 +47,61 @@ int main(int argc, char **argv)
         int type;
         *inp >> type;
         if (type) e++;
-        tests[type].insert(name);
-        testa.insert({ name, type });
+        tests.insert({ name, type });
 
         char *p;
-        int val = strtol(name.c_str(), &p, 10);
-        if (*p == 0 && to_string(val) == name && val < n) good.push_back(name);
+        int val = int(strtol(name.c_str(), &p, 10));
+        if (*p == 0 && to_string(val) == name && val <= n) good.push_back(name);
     }
 
     set<string> ans[2];
     for (int i = 1; i <= n; ++i) ans[i <= e].insert(to_string(i));
 
-<<<<<<< Updated upstream
-    int free[2] = { e, n - e };
     const int limits[2][2] = {{ e + 1, n }, { 1, e }};
     for (const auto &name : good) {
-        int type = testa[name];
+        int type = tests[name];
         int val = stoi(name);
         if (val >= limits[type][0] && val <= limits[type][1]) {
-            testa.erase(testa.find(name));
-            tests[type].erase(tests[type].find(name));
-            free[type]--;
+            tests.erase(tests.find(name));
             ans[type].erase(ans[type].find(name));
         } else {
             bad[type].push_back(name);
+            auto it = ans[0].find(name);
+            if (it != ans[0].end()) ans[0].erase(it);
+            it = ans[1].find(name);
+            if (it != ans[1].end()) ans[1].erase(it);
         }
-=======
-    list<string> script;
-    size_t ce = 0, cn = e + 1;
-    for (size_t i = 0; i < n; ++i) {
-
->>>>>>> Stashed changes
     }
 
+
     while (!bad[0].empty() || !bad[1].empty()) {
-        int type = ans[0].size() > ans[1].size();
+        int type = ans[0].size() < ans[1].size();
         if (bad[type].empty()) type = int(!type);
         string name = bad[type].front();
+        tests.erase(tests.find(name));
         bad[type].pop_front();
         if (ans[type].empty()) {
-            testa.erase(testa.find(name));
-            tests[type].erase(tests[type].find(name));
-            testa.insert({ to_string(1), type });
-//            tests[type].insert()
-cerr << "error1";
+            string newname = to_string(n + 1);
+            print(name, newname);
+            tests.insert({ newname, type });
         } else {
             auto it = ans[type].begin();
             string index = *it;
             ans[type].erase(it);
             print(name, index);
         }
-
-        testa.erase(testa.find(name));
-        tests[type].erase(tests[type].find(name));
-        free[type]--;
+        ans[!type].insert(name);
     }
 
-    for (const auto &kv : testa) {
-        if (ans[kv.second].empty()) {
-cerr << "error2";
-        }
+    for (const auto &kv : tests) {
         auto it = ans[kv.second].begin();
         string index = *it;
         ans[kv.second].erase(it);
         print(kv.first, index);
     }
 
-    //    1 < 3.1           1. move 3 1
-    //    2 = 2.1
-    //    3 < 99.0          4. move 99 3
-    //    4 < 2extra.0      3. move 2extra 4
-    //    5 < 01.0          2. move 01 5
+    cout << script.size() << endl;
+    for (const auto &line : script) cout << line << endl;
 
 
 #ifdef DEBUG_DEV
